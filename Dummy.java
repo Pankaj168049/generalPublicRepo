870eb3e3ac968016c1db8c03ce108d6f55d2de79
Pankaj Ghode <85480602+Pankaj168049@users.noreply.github.com>
2026-03-27 21:31:05 +0530

    Implement dynamic programming solution in Solution class
    Added a new class Solution with a method to solve a problem using dynamic programming.

--- Dummy.java
+++ Dummy.java
@@ -14,3 +14,25 @@ public class Solution {
         return dp[C];
     }
 }
+----------+
+
+public class Solution {
+    public int solve(int A, int[] B, int[] C) {
+        int n = B.length;
+        
+        // dp[w] = max value for capacity w
+        int[] dp = new int[A + 1];
+        
+        // Loop over all capacities
+        for (int w = 0; w <= A; w++) {
+            // Try picking every item
+            for (int i = 0; i < n; i++) {
+                if (C[i] <= w) {
+                    dp[w] = Math.max(dp[w], dp[w - C[i]] + B[i]);
+                }
+            }
+        }
+        
+        return dp[A];
+    }
+}
