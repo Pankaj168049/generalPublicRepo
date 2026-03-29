234b54014af6c8d54cf0b3685a2a4a4564933320
Pankaj Ghode <85480602+Pankaj168049@users.noreply.github.com>
2026-03-29 22:49:58 +0530

    Update Dummy.java

--- Dummy.java
+++ Dummy.java
@@ -36,3 +36,53 @@ public class Solution {
         return dp[A];
     }
 }
+----------
+
+import java.util.*;
+
+public class Solution {
+    class Item {
+        int value;
+        int weight;
+        double ratio;
+
+        Item(int v, int w) {
+            value = v;
+            weight = w;
+            ratio = (double) v / w;
+        }
+    }
+
+    public int solve(int[] A, int[] B, int C) {
+        int n = A.length;
+
+        Item[] items = new Item[n];
+
+        // Create items
+        for (int i = 0; i < n; i++) {
+            items[i] = new Item(A[i], B[i]);
+        }
+
+        // Sort by ratio descending
+        Arrays.sort(items, (a, b) -> Double.compare(b.ratio, a.ratio));
+
+        double totalValue = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            if (C == 0) break;
+
+            if (items[i].weight <= C) {
+                // Take full item
+                totalValue += items[i].value;
+                C -= items[i].weight;
+            } else {
+                // Take fraction
+                totalValue += items[i].ratio * C;
+                C = 0;
+            }
+        }
+
+        // Return floor(ans * 100)
+        return (int) (totalValue * 100);
+    }
+}
